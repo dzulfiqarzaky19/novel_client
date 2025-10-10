56ba579d2062ee54326b34ad0f0152d6a0d004af
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2025-10-10 11:00:55 +0700

    novel_client: update api

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = (import.meta.env.VITE_API as string) ?? 'http://localhost:3000';
+const baseURL = import.meta.env.VITE_API as string;
 
 export const api = axios.create({
   baseURL,
