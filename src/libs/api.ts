ccf9e297191986c7f4cbd7acc0ba43d73e6e9f4c
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2025-10-10 10:15:31 +0700

    novel_client: create env.example, fix tsconfig

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-  (import.meta.env.VITE_API_URL as string) ?? 'http://localhost:3000';
+  (import.meta.env.BASE_API_URL as string) ?? 'http://localhost:3000';
 
 export const api = axios.create({
   baseURL,
